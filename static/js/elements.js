35711e26263204ba66c39bcc7943b7ac670831d9
AmjadSoufi <113971567+AmjadSoufi@users.noreply.github.com>
2025-05-04 13:07:33 +0200

    update the code with BEM (Block Element Modifier) methodology

--- static/js/elements.js
+++ static/js/elements.js
@@ -1,14 +1,34 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('nav ul');
+const hamburger = document.querySelector(".header__hamburger");
+const navbar = document.querySelector(".header__navbar");
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    hamburger.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
 });
 
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('nav') && !event.target.closest('.hamburger')) {
-        navLinks.classList.remove('active');
-        hamburger.classList.remove('active');
-    }
+document.querySelectorAll(".header__navbar a").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navbar.classList.remove("active");
+  })
+);
+
+const themeToggle = document.getElementById("theme-toggle");
+
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark" || (!savedTheme && prefersDark.matches)) {
+  document.documentElement.setAttribute("data-theme", "dark");
+  themeToggle.checked = true;
+}
+
+themeToggle.addEventListener("change", function () {
+  if (this.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
 });
